2b60304cf4d81ab8831d377f777b360ba46edfa2
18-08 <samakovra@gmail.com>
2020-05-26 07:23:40 +0200

    Neuerungen 26.05.20

--- ProjektPlanung/Controllers/BenutzerController.cs
+++ ProjektPlanung/Controllers/BenutzerController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using ProjektPlanung.Models;
 
 namespace ProjektPlanung.Controllers
@@ -75,8 +76,94 @@ namespace ProjektPlanung.Controllers
             return View(Benutzer);
         }
 
+        //Verify Account
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)
+        {
+            bool Status = false;
 
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                db.Configuration.ValidateOnSaveEnabled = false;
+                var v = db.Benutzers.Where(a => a.AktivierungsCode == new Guid(id)).FirstOrDefault();
 
+                if (v != null)
+                {
+                    v.istBestaetigt = true;
+                    db.SaveChanges();
+                    Status = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Ungültige Anfrage";
+                }
+            }
+
+            ViewBag.Status = Status;
+            return View();
+        }
+
+        //Login
+        [HttpGet]
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        //Login POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LogIn login, string ReturnUrl = "")
+        {
+            string message = "";
+
+            using (DatabaseEntities db = new DatabaseEntities())
+            {
+                var v = db.Benutzers.Where(a => a.Mail == login.Mail).FirstOrDefault();
+                if(v != null)
+                {
+                    if(string.Compare(Crypto.Hash(login.Passwort), v.Passwort) == 0)
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20;
+                        var ticket = new FormsAuthenticationTicket(login.Mail, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+                        if(Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Passwort falsch";
+                    }
+                }
+                else
+                {
+                    message = "Email Adresse oder Passwort falsch";
+                }
+            }
+
+            ViewBag.Message = message;
+            return View();
+        }
+
+        //LogOut
+        [Authorize]
+        [HttpPost]
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Benutzer");
+        }
 
         [NonAction]
         public bool ExistiertEmail(string mail)
@@ -98,7 +185,7 @@ namespace ProjektPlanung.Controllers
             var toEmail = new MailAddress(mail);
             var fromEmailPasswort = "Einsatzplanung"; /////////REEEEEEEEEEEEEEEEEEEPPPPPPPPPPPPPLLLLLLLLLLLLLLLAAAAAAAAAAAAAAACCCCCCCCCCEEEEEEEEEEEEEEEEE
             string subject = "Dein Account wurde erfolgreich erstellt!";
-            string body = "<br/><br/>Diese Email bestätigt die erfolgreiche Erstellung deines Accounts bei der Projekt Planung.<br/>Bitte klicke auf den folgenden Link um den Account zu bestätigen.";
+            string body = "<br/><br/>Diese Email bestätigt die erfolgreiche Erstellung deines Accounts bei der Projekt Planung.<br/>Bitte klicke auf den folgenden Link um den Account zu bestätigen.<br/><a href='"+link+"'>"+link+"</a>";
             var smtp = new SmtpClient()
             {
                 Host = "smtp.gmail.com",
---
+++ ProjektPlanung/Models/LogIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjektPlanung.Models
+{
+    public class LogIn
+    {
+        [Display(Name = "Email Adresse")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email Adresse benötigt!")]
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+
+        [Display(Name = "Passwort")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Passwort benötigt!")]
+        [DataType(DataType.Password)]
+        public string Passwort { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
